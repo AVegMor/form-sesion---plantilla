94940503bed4c09229332dee20a44c37b8536ef0
AliciaVegaMoreno <avegmor781@g.educaand.es>
2024-11-15 11:18:33 +0100

    first commit

---
+++ contenido.php
@@ -0,0 +1,13 @@
+<?
+/**
+ * En esta página se encuentra el código necesario para consultar 
+ * si hay un usuario en la sesión y en ese caso se mostrará el contenido 
+ * para usuarios conectados. 
+ * Si no hay usuario en la sesión se mostrará el login.
+ * 
+ * Autor: Nombre Apellidos
+ * 
+ */
+
+
+?>
